fcc051a8cbc2463ab37d428860d2554be1238187
Ms Pickles1014 <177317399+MsPickles1014@users.noreply.github.comgit config --global user.emailgit config --global user.email noeladnelson@gmail.com>
2025-01-29 08:30:48 -0500

    added header.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DummyData from './components/DummyData'
+import './App.css'
+
+function App() {
+  
+
+  return (
+ <>
+<DummyData/>
+ </>
+  )
+}
+
+export default App
---
+++ src/components/DummyData.jsx
@@ -0,0 +1,11 @@
+
+
+
+function DummyData() {
+    return (
+      <div className="bg-gray-500
+      ">dummyDat</div>
+    )
+  }
+  
+  export default DummyData;
